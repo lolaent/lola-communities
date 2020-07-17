a1787a4d6b97a645dfe89b12f58a1d82d678f4ff
Alex Nuta <nutzaalex@gmail.com>
2020-07-17 11:42:13 +0300

    feat: add more next setup and update Readme

---
+++ pages/_app.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import App from 'next/app'
+
+import Nav from '../components/Header'
+import Footer from '../components/Footer'
+import Head from 'next/head'
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <>
+        <Head>
+          <title>{`Lola Communities`}</title>
+          <meta
+            name="Description"
+            content="Lola communities is a place to keep in touch with your favourite tech community"
+          />
+        </Head>
+        <Nav />
+        <main>
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </>
+    )
+  }
+}
+
+export default MyApp
